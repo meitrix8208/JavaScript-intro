1dadc17997e5cdfea3fb6bbf9c9cbeaa0a3119d6
meitrix8208 <maurrod2001@outlook.com>
2022-11-13 22:18:03 -0500

    fix en type module

--- src/data/heroes.js
+++ src/data/heroes.js
@@ -1,29 +1,29 @@
 const heroes = [
     {
         id: 1,
-        name: 'Batman',
-        owner: 'DC'
+        name: "Batman",
+        owner: "DC",
     },
     {
         id: 2,
-        name: 'Spiderman',
-        owner: 'Marvel'
+        name: "SpiderMan",
+        owner: "Marvel",
     },
     {
         id: 3,
-        name: 'Superman',
-        owner: 'DC'
+        name: "Superman",
+        owner: "DC",
     },
     {
         id: 4,
-        name: 'Flash',
-        owner: 'DC'
+        name: "Flash",
+        owner: "DC",
     },
     {
         id: 5,
-        name: 'Wolverine',
-        owner: 'Marvel'
+        name: "Wolverine",
+        owner: "Marvel",
     },
 ];
 
-const owners = ['DC','Marvel'];
+const owners = ["DC", "Marvel"];
